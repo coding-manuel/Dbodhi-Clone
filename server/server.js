8bec6b89da953293a55d8b80db8a59b0eba54e00
coding-manuel <gautam.manuel@gmail.com>
2021-06-16 21:10:48 +0530

    server.js updates

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
+const serveStatic = require('serve-static')
+const path = require('path')
 const connectDB = require('./config/db')
 const productsRoutes = require("./routes/productsRoutes")
 const cartRoutes = require("./routes/cartRoutes")
@@ -14,8 +16,12 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(morgan('tiny'))
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 app.use('/api/products', productsRoutes)
 app.use('/api/cart', cartRoutes)
+app.get(/.*/, function (req, res) {
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)

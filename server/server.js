985a97a8b4c23e43d29fdfd8dab4aa7963148209
coding-manuel <gautam.manuel@gmail.com>
2021-06-17 08:34:23 +0530

    updates to package.json

--- server/server.js
+++ server/server.js
@@ -20,6 +20,10 @@ app.use('/', serveStatic(path.join(__dirname, '/dist')))
 app.use('/api/products', productsRoutes)
 app.use('/api/cart', cartRoutes)
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`)
 });

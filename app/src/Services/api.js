8934109f68cc5daa9c086b540484d33f40be1337
coding-manuel <gautam.manuel@gmail.com>
2021-06-17 09:20:30 +0530

    updates

--- app/src/Services/api.js
+++ app/src/Services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default () => {
   return axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://dbodhi-clone.herokuapp.com/api",
     withCredentials: false,
     headers: {
       Accept: "application/json",
